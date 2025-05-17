1e668d292cdcef69da8254e04791b596b617def0
devpb98 <devpb1998@gmail.com>
2025-05-18 01:54:30 +0530

    Product Service

--- src/main/java/com/devpb/productService/model/Product.java
+++ src/main/java/com/devpb/productService/model/Product.java
@@ -3,11 +3,18 @@ package com.devpb.productService.model;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+
 @Document(value = "product")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 
 }
--- src/main/java/com/devpb/productService/repository/ProductRepository.java
+++ src/main/java/com/devpb/productService/repository/ProductRepository.java
@@ -3,6 +3,6 @@ package com.devpb.productService.repository;
 import com.devpb.productService.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 
 }
